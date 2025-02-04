9831d941b87b6f73d4d949a38288078a16335d39
leechan <dlcks0601@gmail.com>
2025-02-05 02:26:48 +0900

    [Fix] 폴더 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import useAuthStore from '@/store/authStore';
+import React from 'react';
+import useAuthStore from '../store/authStore';
 
 export default function Home() {
   const router = useRouter();
