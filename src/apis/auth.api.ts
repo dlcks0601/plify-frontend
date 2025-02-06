2e23144173d05e7139fdd5a6b33b3d66bdbc8c07
leechan <dlcks0601@gmail.com>
2025-02-06 20:59:06 +0900

    [Refactor] 인증 api 수정

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,11 +1,11 @@
 import fetcher from '@/utils/fetcher';
 import { AxiosResponse } from 'axios';
+import { API_PATH } from './api-path';
 
 interface SignupBody {
   email: string;
   nickname: string;
   password: string;
-  profileUrl?: string;
 }
 
 export interface LoginBody {
@@ -13,38 +13,45 @@ export interface LoginBody {
   password: string;
 }
 
-export interface AuthResponse {
-  access_token: string;
+export interface LoginResponse {
+  user: {
+    userId: number;
+    email: string;
+    nickname: string;
+  };
+  accessToken: string;
 }
 
-export const signup = async (signupBody: SignupBody): Promise<AuthResponse> => {
-  const response: AxiosResponse<AuthResponse> = await fetcher({
+export interface SignupResponse {
+  user: {
+    userId: number;
+    email: string;
+    nickname: string;
+  };
+}
+
+export interface User {
+  userId: number;
+  nickname: string;
+  email: string;
+}
+
+export const signup = async (signupBody: SignupBody) => {
+  const apiPath = API_PATH.signup;
+  const response = await fetcher({
     method: 'POST',
-    url: '/auth/signup',
+    url: apiPath,
     data: signupBody,
   });
   return response.data;
 };
 
-export const login = async (loginBody: LoginBody): Promise<AuthResponse> => {
-  try {
-    const data: AuthResponse = await fetcher({
-      method: 'POST',
-      url: '/auth/login',
-      data: loginBody,
-    });
-
-    console.log('로그인 응답 데이터:', data);
-
-    if (data && data.access_token) {
-      return data;
-    } else {
-      throw new Error('로그인 실패: access_token이 반환되지 않았습니다.');
-    }
-  } catch (error: any) {
-    console.error('로그인 오류:', error.response?.data || error.message);
-    throw new Error(
-      error.response?.data?.message || '로그인 요청 중 오류가 발생했습니다.'
-    );
-  }
+export const login = async (loginBody: LoginBody): Promise<LoginResponse> => {
+  const apiPath = API_PATH.login;
+  const response = await fetcher<{ user: User; accessToken: string }>({
+    method: 'POST',
+    url: apiPath,
+    data: loginBody,
+  });
+  return response.data;
 };
