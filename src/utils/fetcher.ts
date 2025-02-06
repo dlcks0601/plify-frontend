dc440ca0ce0cc1c8663f90e6171264c72358339b
leechan <dlcks0601@gmail.com>
2025-02-06 21:00:19 +0900

    [Refactor] 인증 fetcher 함수 수정

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,4 +1,5 @@
 import axiosInstance from '@/apis/@core';
+import { AxiosResponse } from 'axios';
 
 type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
@@ -11,30 +12,26 @@ export interface FetcherOptions {
 }
 
 export interface FetcherMessage {
+  message: string;
   text?: string;
 }
 
-const fetcher = async <T>(options: FetcherOptions): Promise<T> => {
+const fetcher = async <T>(
+  options: FetcherOptions
+): Promise<AxiosResponse<T & { message: FetcherMessage }, any>> => {
   const { url, method, data, params, headers } = options;
-
   try {
-    const response = await axiosInstance<T>({
+    const response = await axiosInstance<T & { message: FetcherMessage }>({
       url,
       method,
       data,
       params,
       headers,
     });
-    return response.data;
-  } catch (error: any) {
-    // ✅ 서버의 에러 메시지를 콘솔에 표시
-    if (error.response) {
-      console.error('❌ API 오류:', error.response.data.message);
-      throw new Error(error.response.data.message); // 상세한 오류 반환
-    } else {
-      console.error('❌ 요청 실패:', error.message);
-      throw new Error('네트워크 오류 또는 서버에 연결할 수 없습니다.');
-    }
+    return response;
+  } catch (error) {
+    console.error('API 요청 중 오류 발생 :', error);
+    throw error;
   }
 };
 
