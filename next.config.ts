2c15984d1c1653940c0631da5aae491e18fbb1bc
leechan <dlcks0601@gmail.com>
2025-02-12 18:38:51 +0900

    [Chore] 의존성 설치

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: false, // React Strict Mode 비활성화
+  swcMinify: true, // SWC를 사용한 코드 최적화
+  experimental: {
+    appDir: true, // Next.js 14 App Router 사용 시 필요
+  },
 };
 
 export default nextConfig;
