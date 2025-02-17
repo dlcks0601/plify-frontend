0e17bc511c8c62928968f6a3be7356fb6dd61160
leechan <dlcks0601@gmail.com>
2025-02-17 23:24:35 +0900

    [Add] 스포티파이 이미지 모듈 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 const nextConfig = {
   reactStrictMode: false, // React Strict Mode 비활성화
-  swcMinify: true, // SWC를 사용한 코드 최적화
-  experimental: {
-    appDir: true, // Next.js 14 App Router 사용 시 필요
+  images: {
+    domains: [
+      'i.scdn.co',
+      'mosaic.scdn.co',
+      'image-cdn-ak.spotifycdn.com',
+      'image-cdn-fa.spotifycdn.com',
+    ],
   },
 };
 
