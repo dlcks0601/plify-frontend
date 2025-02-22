a66a2e2ee8d5e1cf522314d6ef984fbf02d2c5d9
leechan <dlcks0601@gmail.com>
2025-02-23 03:24:54 +0900

    [Fix] 들고올 자료 개수 수정

--- src/apis/spotify-path.ts
+++ src/apis/spotify-path.ts
@@ -1,6 +1,6 @@
 export const SPOTIFY_TOP_TRACKS_ENDPOINT =
-  'https://api.spotify.com/v1/me/top/tracks?limit=5';
+  'https://api.spotify.com/v1/me/top/tracks?limit=8';
 export const SPOTIFY_TOP_ARTIST_ENDPOINT =
-  'https://api.spotify.com/v1/me/top/artists?limit=5';
+  'https://api.spotify.com/v1/me/top/artists?limit=8';
 export const SPOTIFY_MY_PLAYLISTS =
   'https://api.spotify.com/v1/me/playlists?limit=10';
