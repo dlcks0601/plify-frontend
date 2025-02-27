215a6f96859130991f98853f504db9b39600886a
leechan <dlcks0601@gmail.com>
2025-02-27 14:42:02 +0900

    [Fix] 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '../ui/globals.css';
 import Providers from './components/Providers/Providers';
 import DesktopTopNav from './components/Common/layouts/DesktopTopNav';
 import ThemeProvider from './components/Providers/ThemeProvider';
