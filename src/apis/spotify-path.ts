ac50f4b9fa4726862648dfcd86e5339bbe384f25
leechan <dlcks0601@gmail.com>
2025-03-10 05:22:52 +0900

    [Fix] 스포티파이 패스 반환 수정

--- src/apis/spotify-path.ts
+++ src/apis/spotify-path.ts
@@ -1,6 +1,6 @@
 export const SPOTIFY_TOP_TRACKS_ENDPOINT =
-  'https://api.spotify.com/v1/me/top/tracks?limit=8';
+  'https://api.spotify.com/v1/me/top/tracks?limit=8&time_range=short_term';
 export const SPOTIFY_TOP_ARTIST_ENDPOINT =
-  'https://api.spotify.com/v1/me/top/artists?limit=8';
+  'https://api.spotify.com/v1/me/top/artists?limit=8&time_range=short_term';
 export const SPOTIFY_MY_PLAYLISTS =
   'https://api.spotify.com/v1/me/playlists?limit=10';
