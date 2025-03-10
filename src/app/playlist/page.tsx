77d938f771efd2ce809817e4dc0ff6dd5ea1c6d5
leechan <dlcks0601@gmail.com>
2025-03-11 04:27:01 +0900

    [Feat] 장르 추가

--- src/app/playlist/page.tsx
+++ src/app/playlist/page.tsx
@@ -4,20 +4,27 @@ import PlaylistCard from '@/app/components/Playlist/PlaylistCard';
 import { useState } from 'react';
 import { CiCirclePlus } from 'react-icons/ci';
 import AddPlaylistModal from '../components/Modals/AddPlaylistModal';
+import GenreFilter from '../components/Playlist/GenreFilter';
 
 export default function Playlist() {
   const [isAddPalylistModalOpen, setAddPlaylistModalOpen] = useState(false);
   return (
     <>
       <div className='flex flex-col p-4'>
-        <div className='flex flex-col gap-[20px]'>
+        <div className='flex flex-col gap-[10px]'>
           <div className='flex items-center justify-between'>
-            <div className='text-[80px] font-bold'>Playlist</div>
-            <button onClick={() => setAddPlaylistModalOpen(true)}>
-              <CiCirclePlus size={40} />
+            <div className='text-[80px] font-bold blur-[2px]'>Playlist</div>
+            <button
+              onClick={() => setAddPlaylistModalOpen(true)}
+              className='text-[80px] font-black blur-[2px]'
+            >
+              +
             </button>
           </div>
-          <PlaylistCard />
+          <div className='flex flex-col gap-[30px]'>
+            <GenreFilter />
+            <PlaylistCard />
+          </div>
         </div>
       </div>
       <AddPlaylistModal
